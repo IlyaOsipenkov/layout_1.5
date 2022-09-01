8978bc2419c5646f9537d0c7f181f638342e326d
IlyaOsipenkov <dakarlog@gmail.com>
2022-09-02 02:05:35 +0300

    render of width

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
+let swiper = null
+let screenWidth = window.screen.width
+const isMobile = (screenWidth) => {
+    return screenWidth <= 767
+}
 
-const swiper = new Swiper('.swiper', {
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-    },
-    slidesPerView: 1.3,
-    spaceBetween: 16,
-});
+const slidesPerView = () => {
+    const slide = document.getElementsByClassName('swiper-button')[0].clientWidth
+    return (screenWidth) / (slide + 8)
+}
+
+
+if (isMobile(screenWidth)) {
+    swiper = new Swiper('.swiper', {
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+        },
+        slidesPerView: slidesPerView(),
+        spaceBetween: 16,
+    });
+}
+
+
+const resize = () => {
+    screenWidth = window.screen.width
+    if (swiper != null) {
+        if (isMobile(screenWidth)) {
+            swiper = new Swiper('.swiper', {
+                pagination: {
+                    el: '.swiper-pagination',
+                    type: 'bullets',
+                },
+                slidesPerView: slidesPerView(),
+                spaceBetween: 16,
+            });
+        } else {
+            swiper.destroy(true, true)
+            swiper.disable()
+        }
+    }
+}
+
+window.addEventListener('resize', resize)
+
+
+
+
 
