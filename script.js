5991aede077f1f5018be2cb1cd0ad6e9cbbe4d05
IlyaOsipenkov <dakarlog@gmail.com>
2022-09-05 01:44:47 +0300

    cover-uncover

--- script.js
+++ script.js
@@ -43,6 +43,38 @@ const resize = () => {
 
 window.addEventListener('resize', resize)
 
+let buttonState = false
+
+const buttonMoreInfo = document.querySelector('.button-more-info')
+const extraLinks = document.getElementsByClassName('last-elems-slides')
+
+const clickHandler = (clickEvent) => {
+    const [arrows, text] = buttonMoreInfo.children
+    const toggle = (state) => {
+        if (!state) {
+            text.innerHTML = 'Скрыть'
+            arrows.style.transform = 'rotate(180deg)'
+            buttonMoreInfo.style.top = '400px'
+            Array.from(extraLinks).forEach(el => {
+                el.style.display = 'block'
+            });
+        } else {
+            text.innerHTML = 'Показать все'
+            arrows.style.transform = 'rotate(0)'
+            buttonMoreInfo.style.top = '300px'
+            Array.from(extraLinks).forEach(el => {
+                el.style.display = 'none'
+            });
+        }
+        buttonState = !buttonState
+    }
+    toggle(buttonState)
+}
+
+buttonMoreInfo.addEventListener('click', clickHandler)
+
+
+
 
 
 
